8aeef3458cae2d88d0fb7cdcb879edb6910fe256
jhigger <njohnjhigger@gmail.com>
2022-11-04 15:24:08 +0800

    fix

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,16 +1,11 @@
-import Image from "next/image";
-
 const Dashboard = () => {
 	return (
-		<section id="dashboard" className="relative aspect-[3/1] bg-black">
-			<Image
-				src="/assets/images/banner.png"
-				alt="banner"
-				fill
-				className="opacity-20"
-			/>
-			<div className="absolute top-1/2 left-1/2 m-auto -translate-x-1/2 -translate-y-1/2 transform py-16 px-8">
-				<div className="container mx-auto flex flex-col items-center justify-center">
+		<section
+			id="dashboard"
+			className="relative bg-[url('/assets/images/banner.png')] bg-cover bg-center bg-no-repeat md:aspect-[3/1]"
+		>
+			<div className="h-full bg-black bg-opacity-90 py-16 px-8">
+				<div className="container mx-auto flex h-full flex-col items-center justify-center">
 					<h2 className="my-2 w-full text-center text-3xl font-bold leading-tight text-white lg:text-5xl">
 						Dashboard coming soon for holders
 					</h2>
