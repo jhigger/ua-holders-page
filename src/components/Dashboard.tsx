eb77407cbdeb232b3e299c6644a1fddba5633167
jhigger <njohnjhigger@gmail.com>
2022-11-29 14:10:41 +0800

    change image

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,21 +1,18 @@
+import Image from "next/image";
+
 const Dashboard = () => {
 	return (
 		<section
 			id="dashboard"
-			className="relative bg-[url('/assets/images/banner.png')] bg-cover bg-center bg-no-repeat md:aspect-[3/1]"
+			className="container mx-auto flex h-full flex-col items-center justify-center"
 		>
-			<div className="h-full bg-black bg-opacity-80 py-16 px-8">
-				<div className="container mx-auto flex h-full flex-col items-center justify-center">
-					<h2 className="my-2 w-full text-center text-3xl font-bold leading-tight text-white lg:text-5xl">
-						Dashboard coming soon for holders
-					</h2>
-					<p className="my-2 w-full text-center text-xl leading-tight text-white">
-						Dashboard will cater to multiple tools provided to our
-						holders, including NFT tools, sniper tools, insights,
-						and more
-					</p>
-				</div>
-			</div>
+			<Image
+				src="/assets/images/roadmap.png"
+				alt="roadmap"
+				width={1344}
+				height={695}
+				quality={100}
+			/>
 		</section>
 	);
 };
